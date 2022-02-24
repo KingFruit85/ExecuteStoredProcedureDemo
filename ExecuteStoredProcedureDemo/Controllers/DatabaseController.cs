553c14d1637b65fa68a109d6ac19cc9e87ce2e27
KingFruit85 <christopher.aaron.long@gmail.com>
2022-02-24 23:25:50 +0000

    added a put

--- ExecuteStoredProcedureDemo/Controllers/DatabaseController.cs
+++ ExecuteStoredProcedureDemo/Controllers/DatabaseController.cs
@@ -38,17 +38,36 @@ namespace ExecuteStoredProcedureDemo.Controllers
                         int StoreAvalibility = (int)reader[4];
 
                         result.Add(new Cake(CakeType, Cost, Description, Weight, StoreAvalibility));
-
                     }
                 }
             }
             return result;
         }
 
-        //public Cake GetAllCakesFromDatabase()
-        //{
+        [HttpPut]
+        public void Put(string cakeType, int cost, string description, int weight, int storeAvalibility)
+        {
+            SqlConnection connection = new SqlConnection();
 
-        //}
+            connection.ConnectionString = "Data Source=DESKTOP-8DRN3BN;Initial Catalog=StatusCakeDemo;Integrated Security=True";
+            connection.Open();
+
+            string procedureName = "[dbo].[save_a_cake]";
+
+            using (SqlCommand command = new SqlCommand(procedureName, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.Add(new SqlParameter("@CakeType", cakeType));
+                command.Parameters.Add(new SqlParameter("@Cost", cost));
+                command.Parameters.Add(new SqlParameter("@Description", description));
+                command.Parameters.Add(new SqlParameter("@Weight", weight));
+                command.Parameters.Add(new SqlParameter("@StoreAvalibility", storeAvalibility));
+
+                command.ExecuteNonQuery();
+            }
+
+        }
 
     }
 }
